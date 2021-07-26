401dcc6af1187a7a5cb32b7890d4b08df4f9a2aa
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-07-26 21:53:08 +0100

    sort by ratings query complete

--- src/resolvers/gyms.js
+++ src/resolvers/gyms.js
@@ -6,12 +6,15 @@ const gyms = async (_, { city, sortBy }) => {
       .populate("exerciseFacilities")
       .populate("otherFacilities");
     return gymsFromDb;
-  } else {
-    const gymsFromDb = await Gym.find({})
-      .populate("exerciseFacilities")
-      .populate("otherFacilities");
-    return gymsFromDb;
   }
+  if (sortBy === "rating") {
+    return await Gym.find({}).sort({ [sortBy]: -1 });
+  }
+
+  const gymsFromDb = await Gym.find({})
+    .populate("exerciseFacilities")
+    .populate("otherFacilities");
+  return gymsFromDb;
 };
 
 module.exports = gyms;
