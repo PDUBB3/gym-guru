7b8cd0e1ab014147c3df2180157aa9f0dcde3a28
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-07-30 15:39:11 +0100

    added user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,63 @@
 const { Schema, model } = require("mongoose");
 
-const schema = {};
+const schema = {
+  firstName = {
+    type: String,
+    required: true,
+  },
+
+  lastName = {
+    type: String,
+    required: true,
+  },
+
+  email = {
+    type: String,
+    required: true,
+  },
+
+  username = {
+    type: String,
+    required: true,
+  },
+
+  password = {
+    type: String,
+    required: true,
+  },
+
+  gymId = {
+    type: Schema.Types.ObjectId,
+    ref: "Gym",
+  },
+
+  profileImage = {
+    type: String,
+  },
+
+  city = {
+    type: String,
+  },
+
+  bio = {
+    type: String,
+  },
+
+  goals = {
+    type: [String]
+  },
+
+  interests = {
+    type: [String]
+  },
+
+  buddies = [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }
+  ]
+};
 
 const userSchema = new Schema(schema);
 
