6721ecbc42ab7a35e66239609992695bf544e87e
Leon <leonwheeler08@gmail.com>
2021-08-17 20:17:09 +0100

    Added populate to findUser resolver.

--- src/resolvers/findUser.js
+++ src/resolvers/findUser.js
@@ -1,7 +1,9 @@
 const { User } = require("../models");
 
 const findUser = async (_, { username }) => {
-  const user = await User.findOne({ username }).populate("buddies");
+  const user = await User.findOne({ username })
+    .populate("buddies")
+    .populate("attendingGymId");
   return user;
 };
 
