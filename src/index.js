1ea4258c44970cc35880c81207f231c0cfbecf79
Leon <leonwheeler08@gmail.com>
2021-08-26 23:20:58 +0100

    Added PORT

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ const resolvers = require("./resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
+const PORT = process.env.PORT || 3001;
+
 db.once("open", () => {
-  server.listen().then(({ url }) => {
+  server.listen({ port: PORT }).then(({ url }) => {
     console.log(`Server ready at ${url}`);
   });
 });
