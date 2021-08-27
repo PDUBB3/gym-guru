7d2336d4a331b40a7966e8cb15b97235fea47bc5
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-27 15:07:10 +0100

    added 3 new users

--- src/data/users.js
+++ src/data/users.js
@@ -29,6 +29,45 @@ const users = [
     goals: ["Zen", "Make Friends"],
     interests: ["Yoga", "Cycling"],
   },
+  {
+    firstName: "Laura",
+    lastName: "Wise",
+    email: "laurawise@email.com",
+    username: "laurawise",
+    password: "password123",
+    profileImageUrl:
+      "https://images.unsplash.com/photo-1584863265045-f9d10ca7fa61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+    city: "Newcastle Upon Tyne",
+    bio: "No pain, no gain!",
+    goals: ["Lose weight", "Toning"],
+    interests: ["Cardio", "Gym Classes"],
+  },
+  {
+    firstName: "Ronald",
+    lastName: "Mcdonald",
+    email: "rondon@email.com",
+    username: "ronaldmcdonald",
+    password: "password123",
+    profileImageUrl:
+      "https://images.unsplash.com/photo-1520813938755-db73a1c31aad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+    city: "Liverpool",
+    bio: "Everyday is a good day to go BEAST MODE",
+    goals: ["Improving Endurance"],
+    interests: ["Weight Lifting"],
+  },
+  {
+    firstName: "Ashley",
+    lastName: "Crow",
+    email: "ashleycrow@email.com",
+    username: "ashleycrow",
+    password: "password123",
+    profileImageUrl:
+      "https://images.unsplash.com/photo-1614091245298-3effbd8ed161?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    city: "Leeds",
+    bio: "On good days work out, on bad days work out harder!",
+    goals: ["Improving Endurance"],
+    interests: ["Weight Lifting"],
+  },
 ];
 
 module.exports = users;
